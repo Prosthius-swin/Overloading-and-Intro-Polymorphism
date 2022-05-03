56cf5555b9301f146ac3945edebc3a47eba22b83
CALLUM HOPKINS <103919468@student.swin.edu.au>
2022-05-03 12:32:50 +1000

    Partial

--- polymorphism-and-overloading/Classes.cs
+++ polymorphism-and-overloading/Classes.cs
@@ -2,8 +2,30 @@ using System;
 
 namespace polymorphism_and_overloading
 {
-    class Engine
+    public abstract class Engine
     {
-        
+        private  int horsePower { get; set; }
+        private int energyStoreMaxSize { get; set; }
+        private int currentEnergyStore { get; set; }
+
+        public Engine(int pHorsePower, int pEnergyStoreMaxSize)
+        {
+            this.horsePower = pHorsePower;
+            this.energyStoreMaxSize = pEnergyStoreMaxSize;
+        }
+        public Engine (){}
+        public void Rev(){}
+        public void Rev(int n1){}
+        public void Refuel(){}
+        public void Refuel(int n1){}
+    }
+    public class InternalCombustionEngine:Engine
+    {
+        public string fuelType { get; set; }
+
+        public InternalCombustionEngine(string pFuelType, int pHorsePower, int pEnergyStoreMaxSize):base(pHorsePower, pEnergyStoreMaxSize)
+        {
+            this.fuelType = pFuelType;
+        }
     }
 }
