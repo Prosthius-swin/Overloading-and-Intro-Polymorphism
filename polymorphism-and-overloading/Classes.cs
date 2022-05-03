c2af2de61dd91ff822b9f2ce9154481c59c39452
CALLUM HOPKINS <103919468@student.swin.edu.au>
2022-05-03 15:34:49 +1000

    Completed

--- polymorphism-and-overloading/Classes.cs
+++ polymorphism-and-overloading/Classes.cs
@@ -72,13 +72,12 @@ namespace polymorphism_and_overloading
             if(!(CurrentEnergyStore + n1 > EnergyStoreMaxSize))
             {
                 CurrentEnergyStore += n1;
-                Console.WriteLine($"Refuelled {n1} litres");
+                Console.WriteLine($"REFUELLED {n1} LITRES");
             } else
             {
                 CurrentEnergyStore = EnergyStoreMaxSize;
                 Console.WriteLine("REFUELED TO FULL");
-            }
-            
+            }   
         }
     }
     public class ElectricEngine:Engine
@@ -88,8 +87,53 @@ namespace polymorphism_and_overloading
         public ElectricEngine(string pBatteryType, int pHorsePower, int pEnergyStoreMaxSize):base(pHorsePower, pEnergyStoreMaxSize)
         {
             this.batteryType = pBatteryType;
-            CurrentEnergyStore = EnergyStoreMaxSize;
+            this.EnergyStoreMaxSize = pEnergyStoreMaxSize;
+            this.CurrentEnergyStore = pEnergyStoreMaxSize;
         }
         public ElectricEngine(){} 
+        public void Rev()
+        {
+            if(CurrentEnergyStore == 0)
+            {
+                Console.WriteLine("FLAT BATTERY");
+            } else
+            {
+                Console.WriteLine("SWWIIIISSSSHHH!");
+                CurrentEnergyStore--;
+            }
+        }
+        public void Rev(int n1)
+        {
+            int counter = 0;
+            while(counter < n1)
+            {
+                if(CurrentEnergyStore == 0)
+                {
+                    Console.WriteLine("FLAT BATTERY");
+                } else
+                {
+                    Console.WriteLine("SWWIIIISSSSHHH!!");
+                    CurrentEnergyStore--;
+                }
+                counter++;
+            }
+        }
+        public void Refuel()
+        {
+            CurrentEnergyStore = EnergyStoreMaxSize;
+            Console.WriteLine("RECHARGED TO FULL");
+        }
+        public void Refuel(int n1)
+        {
+            if(!(CurrentEnergyStore + n1 > EnergyStoreMaxSize))
+            {
+                CurrentEnergyStore += n1;
+                Console.WriteLine($"RECHARGED {n1} AMP HOURS");
+            } else
+            {
+                CurrentEnergyStore = EnergyStoreMaxSize;
+                Console.WriteLine("RECAHRGED TO FULL");
+            }   
+        }
     }
 }
--- polymorphism-and-overloading/Program.cs
+++ polymorphism-and-overloading/Program.cs
@@ -7,8 +7,16 @@ namespace polymorphism_and_overloading
         static void Main(string[] args)
         {
             InternalCombustionEngine ICE1 = new InternalCombustionEngine("Unleaded", 300, 500);
-            ICE1.Rev(500);
-            ICE1.Refuel(500);
+            ICE1.Rev();
+            //ICE1.Rev(500);
+            ICE1.Refuel();
+            //ICE1.Refuel(500);
+
+            ElectricEngine EE1 = new ElectricEngine("Li", 250, 300);
+            EE1.Rev();
+            EE1.Rev(10);
+            EE1.Refuel();
+            EE1.Refuel(50);
         }
     }
 }
